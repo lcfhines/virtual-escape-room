19759e6fea0391734aca331990febe9d31540e57
Danny <dl667@cornell.edu>
2022-12-10 13:20:53 -0500

    client-side signup page minus actual html scripts

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -4,3 +4,43 @@ import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
+const Signup = () => {
+    const [formState, setFormState] = useState({
+        firstName: '',
+        lastName: '',
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    const [addUser, { error, data }] = useMutation(ADD_USER);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const { data } = await addUser({
+                variables: { ...formState },
+            });
+
+            Auth.login(data.addUser.token)
+        } catch (e) {
+            console.log(e)
+        };
+    };
+
+    return (
+        <main>Hello World</main>
+    )
+};
+
+export default Signup;
