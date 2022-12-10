316d491f32846f1d4b735a47ddf8a8a33c4b29e4
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-12-10 13:56:21 -0500

    mutation for add User and login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,6 +10,29 @@ const resolvers = {
 
   },
 
-}
+  Mutation: {
+    addUser: async (parent, { first_name, last_name, email, password }) => {
+      const user = await User.create({ first_name, last_name, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials!');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+} 
    
 module.exports = resolvers;
