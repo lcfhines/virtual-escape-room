a226e307d2116ac26434618997c979562eec5d1b
Michael Nabatov <happyanddebtfree@gmail.com>
2022-12-10 12:39:20 -0500

    Imported all models in models/index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,11 @@
 const User = require('./User');
+const Game = require('./Game');
+const Room = require('./Room');
+const Object = require('./Object');
+const Interaction = require('./Interaction');
+const Reaction = require('./Reaction');
+const Motive = require('./Motive');
+const Solution = require('./Solution');
+const GameUserInteraction = require('./GameUserInteraction');
 
-module.exports = { User };
+module.exports = { User, Game, Room, Object, Interaction, Reaction, Motive, Solution, GameUserInteraction };
