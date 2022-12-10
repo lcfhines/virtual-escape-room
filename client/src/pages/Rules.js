4ba031804b2aa44c8090d212e3041b4ccbefb210
Danny <dl667@cornell.edu>
2022-12-10 16:04:44 -0500

    rules page base code

---
+++ client/src/pages/Rules.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Rules = () => {
+    return (
+        <div>
+            <h1>....TAG LINE .....</h1>
+            <div id="instruction">
+                <h2>How to play</h2>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur,</li>
+                    <li>adipisicing elit. In, nisi modi! Possimus tempore </li>
+                    <li>itaque recusandae praesentium aliquid dolores, quia cum</li>
+                    <li>illo ad eum architecto. Sequi?</li>
+                    <li>ratione eius maxime tempora, iste </li>
+                </ul>
+            </div>
+            <div id="rule">
+                <h2>Rules</h2>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur,</li>
+                    <li>adipisicing elit. In, nisi modi! Possimus tempore </li>
+                    <li>itaque recusandae praesentium aliquid dolores, quia cum</li>
+                    <li>illo ad eum architecto. Sequi?</li>
+                    <li>ratione eius maxime tempora, iste </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Rules;
