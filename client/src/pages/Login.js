ac8f1f255f880ad41a16a25e75083ee1d60a809d
Danny <dl667@cornell.edu>
2022-12-10 13:17:04 -0500

    client-side Login page minus the actual html scripts

---
+++ client/src/pages/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Link } from '@apollo/client';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const Login = (props) => {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error, data }] = useMutation(LOGIN_USER);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const { data } = await login({
+                variables: { ...formState },
+            });
+
+            Auth.login(data.login.token);
+        } catch (e) {
+            console.log(e)
+        };
+
+        setFormState({
+            email: '',
+            password: '',
+        });
+    };
+
+    return (
+        <main>Hello World</main>
+    )
+};
+
+export default Login;
