ee50d448a68285fd56394c8c0b445453aae71bc6
Michael Nabatov <happyanddebtfree@gmail.com>
2022-12-11 16:33:01 -0500

    Added seeds process for all tables

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -100,42 +100,5 @@ const typeDefs = gql`
     endGame(solutionTime: Int): User
   }
 `;
-//startGame - start game in mutation will wipe out game user interaction table for that userId and start coutndown timer, switch room, click on object
-// addUserInteraction - add user interaction, Interactions written to game user interaction table
-// checkSolution - check 3 dropdowns (character id, object id and motive id) against background solution table to see if they match and if so we return true or false
-
-// games: [Game]!
-//     game(game_id: Int!): Game -- gets one game and all rooms
-//     room(roomId: Int!): Room -- gets one room and all objects
-
-//     object(objectId: String!): Object  -- get all interactions and check whether he interacted in order to display or not
-
-    
-//     -- get all characters 
-//     -- get all things that are weapons that he interacted with
-//     -- get all motives from the interactions that he interacted with  
-//     -- get leaderboard    
-    
-    
-    
-//     interaction(interactionId: String!): Interaction
-    
-//     users: [User]!
-//     user(userId: ID!): User
-//     me: User
-
-//     checkUserInteraction(interactionId: String!): GameUserInteraction
-//   }
-
-//   type Mutation {
-//     addUser(first_name: String!, last_name: String!, email: String!, password: String!): Auth
-//     login(email: String!, password: String!): Auth
-    
-//     startGame(user_id: ID!): Game  -- wipe out GameUserInteractions
-//     addUserInteraction(user_id: ID!, interaction_id: ID!): GameUserInteraction -- adds to gameuserinteractions
-//     checkSolution(character_id: ID!, thing_id: ID!, motive_id: ID!): Boolean
-
-//     updateUser(userId: ID!, games_played: Int, best_score: Int): User
-//   }
 
 module.exports = typeDefs;
