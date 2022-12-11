6d338043fa8def371a3366ac1a1ee30ba1079e73
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-12-11 13:10:21 -0500

    added Home.js page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home() => {
+    return (
+        <main>
+            <h1>Virtual Escape Room</h1>
+            <div id="home">
+                <h2>Intro</h2>
+                <h2>Leader Board</h2>
+                <div id="play"><a href="#">PLAY</a></div>
+                <h2>How to Play / Rules</h2>
+                </div>
+        </main>
+    )
+}
