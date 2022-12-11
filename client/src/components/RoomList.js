2b88a03c5eb5bb27e4e59ba87241c8bef0d5f33b
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-12-11 17:29:49 -0500

    saving changes

--- client/src/components/RoomList.js
+++ client/src/components/RoomList.js
@@ -6,7 +6,15 @@ const RoomList = ({ rooms }) => {
 
     return (
         <div>
-        
+         <div>
+            {rooms && rooms.map((room) => (
+                <div key={room._id}>
+                    <Link to={`/room`}>
+                        <p>{rooms.title}</p>
+                    </Link>
+                </div>
+            ))}
+            </div>
         </div>
     )
 };
