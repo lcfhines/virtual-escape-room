2311c8eabf44dafd648e8ae1ab26f38c97d9b4d8
Danny <dl667@cornell.edu>
2022-12-11 13:16:07 -0500

    rules page

--- client/src/pages/Rules.js
+++ client/src/pages/Rules.js
@@ -3,25 +3,24 @@ import React from 'react';
 const Rules = () => {
     return (
         <div>
-            <h1>....TAG LINE .....</h1>
+            <h1>Do you dare to escape where countless have tried and failed?</h1>
             <div id="instruction">
                 <h2>How to play</h2>
                 <ul>
-                    <li>Lorem ipsum dolor sit amet consectetur,</li>
-                    <li>adipisicing elit. In, nisi modi! Possimus tempore </li>
-                    <li>itaque recusandae praesentium aliquid dolores, quia cum</li>
-                    <li>illo ad eum architecto. Sequi?</li>
-                    <li>ratione eius maxime tempora, iste </li>
+                    <li>Choose which room you would like to enter</li>
+                    <li>Read the scenario provided</li>
+                    <li>Once you are in the room, interact with the various objects and/or people to gather clues</li>
+                    <li>Depending on your interactions, you may unlock hidden paths or further interactions, so look out!</li>
+                    <li>In order to escape the room, return to the starting location and provide your answer. If you are right, you escape; if you are wrong, well, let us hope you are right!</li>
                 </ul>
             </div>
             <div id="rule">
                 <h2>Rules</h2>
                 <ul>
-                    <li>Lorem ipsum dolor sit amet consectetur,</li>
-                    <li>adipisicing elit. In, nisi modi! Possimus tempore </li>
-                    <li>itaque recusandae praesentium aliquid dolores, quia cum</li>
-                    <li>illo ad eum architecto. Sequi?</li>
-                    <li>ratione eius maxime tempora, iste </li>
+                    <li>You start in a room</li>
+                    <li>Explore and interact to gather clues</li>
+                    <li>You have 'x' interactions and 'y' time to complete the challenge</li>
+                    <li>Submit your answer in the starting room</li>
                 </ul>
             </div>
         </div>
