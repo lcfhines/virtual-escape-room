8f2f88091d1fb8b9d7e7549ba02cf77e625c7d41
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-12-11 13:32:39 -0500

    exported GameList

--- client/src/components/GameList.js
+++ client/src/components/GameList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const GameList = ({ games }) => {
     if (!games.length) {
@@ -15,4 +15,6 @@ const GameList = ({ games }) => {
             ))}
         </div>
     )
-}
+};
+
+export default GameList;
