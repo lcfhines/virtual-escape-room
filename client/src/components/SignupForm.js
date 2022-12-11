e0bcf1a6684bab00127b42ce724aee7bb81e64a8
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-12-11 12:14:47 -0500

    worked on installations in client package.json

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
+// import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -12,7 +12,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [addUser, { error, data }] = useMutation(ADD_USER);
+  // const [addUser, { error, data }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -29,16 +29,16 @@ const SignupForm = () => {
       event.stopPropagation();
     }
 
-    try {
-      const { data } = await addUser({
-        variables: { ...userFormData },
-      });
+    // try {
+    // //   const { data } = await addUser({
+    // //     variables: { ...userFormData },
+    // //   });
 
-      Auth.login(data.addUser.token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
+    //   Auth.login(data.addUser.token);
+    // } catch (err) {
+    //   console.error(err);
+    //   setShowAlert(true);
+    // }
 
     setUserFormData({
       email: '',
