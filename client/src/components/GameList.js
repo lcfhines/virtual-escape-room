ff505b241a3e7e3da329a7850f5872cc79c42518
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-12-11 14:28:34 -0500

    added logged in validations to Gamelist.js

--- client/src/components/GameList.js
+++ client/src/components/GameList.js
@@ -1,20 +1,48 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER , GET_ME } from '../utils/queries';
+import Auth from '../utils/auth';
 
 const GameList = ({ games }) => {
-    if (!games.length) {
-        return <h3>No games available yet!</h3>;
-    }
+    const { _id: userParam } = useParams();
 
-    return (
-        <div>
+    const { loading, data } = useQuery(userParam ? QUERY_USER : GET_ME, {
+      variables: { _id: userParam },
+    });
+  
+    const user = data?.me || data?.user || {};
+
+    if (Auth.loggedIn() && Auth.getProfile().data._id === userParam) {
+        // if (!games.length) {
+        //     return <h3>No games available yet!</h3>;
+        // }
+        return (
+            <div>
             {games && games.map((game) => (
                 <div key={game._id}>
                     <p><a href="#">{games.title}</a></p>
                 </div>
             ))}
         </div>
-    )
+        )
+      }
+
+      if (loading) {
+        return <div>Loading...</div>;
+      }
+
+      if (!user?._id) {
+        return (
+          <h4>
+            You need to be logged in to see this. Use the navigation links above to sign up or log in!
+          </h4>
+        );
+      }
+    
+
+
 };
 
 export default GameList;
