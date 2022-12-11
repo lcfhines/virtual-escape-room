235551c9ddac0fd088bb0bbccd3bfb2adb88a1a1
Danny <dl667@cornell.edu>
2022-12-11 13:46:00 -0500

    login signup and me queries and mutations on frontend

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($first_name: String!, $last_name: String!, $email: String!, $password: String!) {
+        addUser(first_name: $first_name, last_name: $last_name, email: $email, password: $password) {
+            user {
+                _id
+                first_name
+                last_name
+                email
+                password
+            }
+        }
+    }
+`;
