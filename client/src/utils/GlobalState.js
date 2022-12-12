c9804280a50d26217a6ca3079bc5aad89f04fd35
Michael Nabatov <happyanddebtfree@gmail.com>
2022-12-12 13:56:03 -0500

    Added GlobalState.js, reducers.js, and actions.js

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,19 @@
+import React, { createContext, useContext } from "react";
+import { useGameReducer } from './reducers'
+
+const GameContext = createContext();
+const { Provider } = GameContext;
+
+const GameProvider = ({ value = [], ...props }) => {
+  const [state, dispatch] = useGameReducer({
+    rooms: [],
+  });
+
+  return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useGameContext = () => {
+  return useContext(GameContext);
+};
+
+export { GameProvider, useGameContext };
