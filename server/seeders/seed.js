c19b24b77a6f7d9a1c61bc527a3f88b8b26f8e3e
Goat <dpajbk@gmail.com>
2022-12-13 10:15:24 -0500

    added comments to seed.js

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -5,7 +5,7 @@ const gameSeeds = require('./games.json');
 const roomSeeds = require('./rooms.json');
 const objectSeeds = require('./objects.json');
 const interactionSeeds = require('./interactions.json');
-const motiveSeeds = require ('./motive.json');
+const motiveSeeds = require('./motive.json');
 // const solutionLetterSeeds = require('./solutionLetter.json');
 // const solutionSeeds = require('./solution.json')
 
@@ -24,47 +24,49 @@ db.once('open', async () => {
     // const solutionLetter = await SolutionLetter.insertMany(solutionLetterSeeds)
     const motives = await Motive.insertMany(motiveSeeds);
 
-    let interactions = interactionSeeds.map(interaction=> {
-      const motiveIds = 
+
+    // apply motives to interactions
+    let interactions = interactionSeeds.map(interaction => {
+      const motiveIds =
         motives
-            .filter(motive => motive.interaction_id === interaction.interaction_id)
-            .map(motive => motive._id);
-        
-      return {...interaction, motives: motiveIds}
+          .filter(motive => motive.interaction_id === interaction.interaction_id)
+          .map(motive => motive._id);
+
+      return { ...interaction, motives: motiveIds }
     })
     interactions = await Interaction.insertMany(interactions);
 
-
-    let objects = objectSeeds.map(object=> {
-      const interactionIds = 
+    // apply interactions to objects
+    let objects = objectSeeds.map(object => {
+      const interactionIds =
         motives
-            .filter(interaction => interaction.object_id === object.object_id)
-            .map(interaction => interaction._id); 
-      
-      return {...object, interactions: interactionIds}
+          .filter(interaction => interaction.object_id === object.object_id)
+          .map(interaction => interaction._id);
+
+      return { ...object, interactions: interactionIds }
     })
     objects = await Object.insertMany(objects);
 
-
-    let rooms = roomSeeds.map(room=> {
-      const objectIds = 
-      objects
+    // apply objects to rooms
+    let rooms = roomSeeds.map(room => {
+      const objectIds =
+        objects
           .filter(object => object.room_id === room.room_id)
           .map(object => object._id);
-      
-        return {...room, objects: objectIds}
+
+      return { ...room, objects: objectIds }
     })
     rooms = await Room.insertMany(rooms);
 
 
     // create an array of games, with generated room_ids
-    let games = gameSeeds.map(game=> {
-      const roomIds = 
+    let games = gameSeeds.map(game => {
+      const roomIds =
         rooms
           .filter(room => room.game_id === game.game_id)
           .map(room => room._id);
-      
-      return {...game, rooms: roomIds}
+
+      return { ...game, rooms: roomIds }
     })
     games = await Game.insertMany(games);
 
