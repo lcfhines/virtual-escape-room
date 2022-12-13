bf67ac770841675decd8ee3747a958c274137953
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-12-13 09:57:21 -0500

    updated roomlist to use global state

--- client/src/components/RoomList.js
+++ client/src/components/RoomList.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useGameContext } from '../utils/GlobalState';
 
-const RoomList = ({ rooms }) => {
- 
+const RoomList = () => {
+    const [state, dispatch] = useGameContext()
     return (
         <div>
          <div>
-            {rooms && rooms.map((room) => (
+            {state.rooms && state.rooms.map((room) => (
                 <div key={room._id}>
-                    <Link to={`/room`}>
-                        <p>{rooms.title}</p>
+                    <Link to={`/room/${room.room_id}`}>
+                        <p>{room.title}</p>
                     </Link>
                 </div>
             ))}
