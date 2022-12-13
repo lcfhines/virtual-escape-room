f017fa695409dc6e88c29d3f021eba52afff1bfc
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-12-13 11:05:08 -0500

    added Object component

---
+++ client/src/components/Object.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useGameContext } from '../utils/GlobalState'
+
+const Object = () => {
+    const [state] = useGameContext();
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
