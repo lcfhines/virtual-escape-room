de7e3b774c9d6d2b591db6f2294b0341966aceb7
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-12-13 20:34:56 -0500

    removed duplicative and unnecessary code in Object.js

--- client/src/components/Object.js
+++ client/src/components/Object.js
@@ -3,64 +3,15 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { useGameContext } from '../utils/GlobalState';
 import InteractionList from './InteractionList';
-import { useParams } from 'react-router-dom';
-
-// const ObjectModal = (props, object, idx) => {
-
-    // const {object_id} = useParams();
-    // const object = state.objects?.find(object => object.object_id === parseInt(object_id)) || {};
-    // console.log(object);
-    // console.log(object_id);
-
-//     return (
-//         <Modal
-//         {...props}
-//         size="lg"
-//         aria-labelledby="contained-modal-title-vcenter"
-//         centered
-//         object={object}
-//         key={idx}
-//       >
-//         <Modal.Header closeButton>
-//           <Modal.Title id="object-modal">
-//             insert object name here
-//             {object.name}
-//           </Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>
-//           <p>insert object description here</p>
-//           {/* {state.object.interactions && state.object.interactions.map((interaction, idx) => (
-//                 <div>
-//                     <InteractionList key={idx} interaction={interaction}/>
-//                 </div>
-//           ))} */}
-//           <div>
-//             <InteractionList />
-//           </div>
-          
-//         </Modal.Body>
-//         <Modal.Footer>
-//           <Button onClick={props.onHide}>Close</Button>
-//         </Modal.Footer>
-//       </Modal>
-//     )
-// }
 
 const Object = ({object}) => {
     const [modalShow, setModalShow] = useState(false);
     const [state] = useGameContext();
-    // const handleClose = () => setModalShow(false);
-    // const handleShow = () => setModalShow(true);
     
     return (
     <>
         <Button variant="outline-danger" size="lg"onClick={() => setModalShow(true)}>{object.name}
         </Button>
-        {/* <ObjectModal 
-        object={object}
-        show={modalShow} 
-        onHide={() => setModalShow(false)}
-        /> */}
         <Modal
         size="lg"
         aria-labelledby="contained-modal-title-vcenter"
@@ -75,15 +26,14 @@ const Object = ({object}) => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {/* <p>{object.description}</p> */}
-          {/* {state.object.interactions && state.object.interactions.map((interaction, idx) => (
+          {/* {state.interactions.map((interaction, idx) => (
                 <div>
                     <InteractionList key={idx} interaction={interaction}/>
                 </div>
           ))} */}
-          {/* <div>
+          <div>
             <InteractionList />
-          </div> */}
+          </div>
           
         </Modal.Body>
         <Modal.Footer>
