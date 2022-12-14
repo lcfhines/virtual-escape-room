4784012cf9ad6405d1f4c1946c3edcdac9180d71
Goat <dpajbk@gmail.com>
2022-12-14 15:04:49 -0500

    fixed timer formatting

--- client/src/pages/Room.js
+++ client/src/pages/Room.js
@@ -14,30 +14,42 @@ const Room = () => {
      const [showModal, setShowModal] = useState(false)
      const { room_id } = useParams();
      const room = state.rooms?.find(room => room.room_id === parseInt(room_id)) || {};
-     console.log(room);
-     console.log(room_id);
+
+     const [timeLeft, setTimeLeft] = useState(state.game.time_limit * 60);
+     const [timerRunning, setTimerRunning] = useState(true);
+
+     const timeHandler = () => {
+          setTimeLeft(timeLeft - 1);
+     }
 
      return (
           <main>
                <div id="room">
-                    {/* {<div className ="map">
-                         <img src="https://raw.githubusercontent.com/mikeyboxx/virtual-escape-room/main/assets/LM-Full.jpg" className="mapimg" alt="manor map"/>
-                         <svg viewBox="0 0 2048 2048" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M 125 250 h 800 v 550 h -800 L 125 250" />
-                              <path d="M 125 850 h 800 v 550 h -175 v 650 h -625 L 125 850" />
+                    {/* <div className ="map">
+                         <img src="https://raw.githubusercontent.com/mikeyboxx/virtual-escape-room/main/assets/LM-Full.jpg" alt="manor map"/>
+                         <svg width="2048" height="2048" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M 100 100 H 900 V 900 H 100 L 100 100" />
+
+                              <circle cx="10" cy="10" r="2" fill="red" />
+                              <circle cx="90" cy="90" r="2" fill="red" />
+                              <circle cx="90" cy="10" r="2" fill="red" />
+                              <circle cx="10" cy="90" r="2" fill="red" />
                          </svg>
-                    </div>} */}
+                    </div> */}
                     <div id="titleTimer">
                          <div>
-                              <h1>{room.title}</h1>
+                              <h2>{room.title}</h2>
                               <RoomList />
                          </div>
-                         <div>
-                              <h2 id="timer">Time Remaining</h2>
-                              <Timer />
+                         <div id = "timerBlock">
+                              <h2 id = "timer">Time Remaining</h2>
+                              <Timer
+                                   timeLeft={timeLeft}
+                                   timerRunning={timerRunning}
+                                   timeHandler={timeHandler}
+                              />
                          </div>
                     </div>
-
                </div>
                <div id="room-desc">
                     <h2>Description of the {room.title}</h2>
@@ -51,14 +63,18 @@ const Room = () => {
                {parseInt(room_id) === state.defaultRoomId
                     && (
                          <div>
-                              <Link onClick={() => setShowModal(true)}> I know who did it! </Link>
+                              <Link onClick={() => {
+                                   setShowModal(true);
+                              }}> I know who did it! Report to the constable your findings to escape the Llewellyn Manor </Link>
                               <Modal
                                    show={showModal}
                                    onHide={() => setShowModal(false)}
                                    size="lg"
                                    aria-labelledby="contained-modal-title-vcenter"
                                    centered>
-                                   < SolutionForm />
+                                   < SolutionForm
+                                        setTimerRunning={setTimerRunning}
+                                        timeLeft={timeLeft} />
 
                               </Modal>
                          </div>)
