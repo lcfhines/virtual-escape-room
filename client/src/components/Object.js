ff479e01ebf8b8f89bde4d8d3abe9247774fb094
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-12-13 20:30:28 -0500

    refactored code to include Modal in the Object function, rendering object.name correctly on the modal

--- client/src/components/Object.js
+++ client/src/components/Object.js
@@ -5,49 +5,50 @@ import { useGameContext } from '../utils/GlobalState';
 import InteractionList from './InteractionList';
 import { useParams } from 'react-router-dom';
 
-const ObjectModal = (props, object, idx) => {
-    const [state] = useGameContext();
+// const ObjectModal = (props, object, idx) => {
+
     // const {object_id} = useParams();
     // const object = state.objects?.find(object => object.object_id === parseInt(object_id)) || {};
     // console.log(object);
     // console.log(object_id);
 
-    return (
-        <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        object={object}
-        key={idx}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="object-modal">
-            insert object name here
-            {object.name}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>insert object description here</p>
-          {/* {state.object.interactions && state.object.interactions.map((interaction, idx) => (
-                <div>
-                    <InteractionList key={idx} interaction={interaction}/>
-                </div>
-          ))} */}
-          <div>
-            <InteractionList />
-          </div>
+//     return (
+//         <Modal
+//         {...props}
+//         size="lg"
+//         aria-labelledby="contained-modal-title-vcenter"
+//         centered
+//         object={object}
+//         key={idx}
+//       >
+//         <Modal.Header closeButton>
+//           <Modal.Title id="object-modal">
+//             insert object name here
+//             {object.name}
+//           </Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>
+//           <p>insert object description here</p>
+//           {/* {state.object.interactions && state.object.interactions.map((interaction, idx) => (
+//                 <div>
+//                     <InteractionList key={idx} interaction={interaction}/>
+//                 </div>
+//           ))} */}
+//           <div>
+//             <InteractionList />
+//           </div>
           
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    )
-}
+//         </Modal.Body>
+//         <Modal.Footer>
+//           <Button onClick={props.onHide}>Close</Button>
+//         </Modal.Footer>
+//       </Modal>
+//     )
+// }
 
 const Object = ({object}) => {
     const [modalShow, setModalShow] = useState(false);
+    const [state] = useGameContext();
     // const handleClose = () => setModalShow(false);
     // const handleShow = () => setModalShow(true);
     
@@ -55,11 +56,40 @@ const Object = ({object}) => {
     <>
         <Button variant="outline-danger" size="lg"onClick={() => setModalShow(true)}>{object.name}
         </Button>
-        <ObjectModal 
+        {/* <ObjectModal 
         object={object}
         show={modalShow} 
         onHide={() => setModalShow(false)}
-        />
+        /> */}
+        <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        object={object}
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        >
+        <Modal.Header closeButton>
+          <Modal.Title id="object-modal">
+            {object.name}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* <p>{object.description}</p> */}
+          {/* {state.object.interactions && state.object.interactions.map((interaction, idx) => (
+                <div>
+                    <InteractionList key={idx} interaction={interaction}/>
+                </div>
+          ))} */}
+          {/* <div>
+            <InteractionList />
+          </div> */}
+          
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={object.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     </>
     )
 }
