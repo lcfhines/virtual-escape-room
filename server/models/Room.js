1b3b9b492e38b92974d3b0b5d3f4ba0e8c5d2e15
Goat <dpajbk@gmail.com>
2022-12-14 13:42:38 -0500

    corrected modifications to schema and seeds

--- server/models/Room.js
+++ server/models/Room.js
@@ -22,9 +22,6 @@ const roomSchema = new Schema(
       type: Number,
       required: true,
     },
-    map: {
-      type: String
-    },
     objects: [
       {
         type:Schema.Types.ObjectId,
